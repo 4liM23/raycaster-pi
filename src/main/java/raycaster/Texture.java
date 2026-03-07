c311f2b501f3192b53ed41132216860116e04304
Zenith <alialmoutawa55@gmail.com>
2026-03-07 03:04:30 +0300

    -no-comment

--- src/main/java/raycaster/Texture.java
+++ src/main/java/raycaster/Texture.java
@@ -9,12 +9,21 @@ public class Texture {
     private final BufferedImage image;
     private final int width;
     private final int height;
+    private final int[] pixels565;
 
     public Texture(String path) {
         try {
             image = ImageIO.read(new File(path));
             width = image.getWidth();
             height = image.getHeight();
+            pixels565 = new int[width * height];
+            int p = 0;
+            for (int r = 0; r < height; r++) {
+                for (int c = 0; c < width; c++) {
+                    pixels565[p++] = PixelBuffer.from32To16Rgb(image.getRGB(c, r));
+                }
+            }
+
         } catch (IOException e) {
             throw new RuntimeException("Failed to load texture: " + path, e);
         }
@@ -29,6 +38,6 @@ public class Texture {
     }
 
     public int getPixel(int x, int y) {
-        return image.getRGB(x, y);
+        return pixels565[width * y + x];
     }
 }
